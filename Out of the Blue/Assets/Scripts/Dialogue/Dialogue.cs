4d09a2f62faed89e8633350b1a701e3e3ca9bb4d
Calum <CalumMcManus7@gmail.com>
2016-11-10 09:07:47 +0000

    *First Commit +All project files

---
+++ Out of the Blue/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dialogue : MonoBehaviour {
+    #region Lists
+    [SerializeField] private List<string> m_Hints = new List<string>();
+    [SerializeField] private List<string> m_Warnings = new List<string>();
+    [SerializeField] private List<string> m_Generic = new List<string>();
+    #endregion
+    #region Randoms
+    public string GetRandomHint()
+    {
+        return m_Hints[Random.Range(0, m_Hints.Count)];
+    }
+    public string GetRandomWarning()
+    {
+        return m_Warnings[Random.Range(0, m_Warnings.Count)];
+    }
+    public string GetRandomGeneric()
+    {
+        return m_Generic[Random.Range(0, m_Generic.Count)];
+    }
+    #endregion
+    #region Specific
+    public string GetHint(int index)
+    {
+        if (index >= m_Hints.Count)
+        {
+            Debug.LogError("Failed to get Hint, Index is out of range");
+            return "";
+        }
+        return m_Hints[index];
+    }
+    public string GetWarning(int index)
+    {
+        if (index >= m_Warnings.Count)
+        {
+            Debug.LogError("Failed to get Warning, Index is out of range");
+            return "";
+        }
+        return m_Warnings[index];
+    }
+    public string GetGeneric(int index)
+    {
+        if (index >= m_Generic.Count)
+        {
+            Debug.LogError("Failed to get Generic, Index is out of range");
+            return "";
+        }
+        return m_Generic[index];
+    }
+    #endregion
+}
---
+++ Out of the Blue/Assets/Scripts/Managers/ManagerContainer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerContainer : MonoBehaviour {
+
+    #region Singleton
+    private static ManagerContainer m_instance;
+    public static ManagerContainer Instance { get { return m_instance; }}
+
+    void Awake()
+    {
+        m_instance = null;
+        m_instance = FindObjectOfType<ManagerContainer>();
+        if(m_instance == null)
+        {
+            Debug.LogError("Failed to find ManagerContainer... Why? Just fucking why?");
+        }
+    }
+    #endregion
+
+    [SerializeField] private Dialogue m_Dialogue;
+    public Dialogue Dialogue { get { return m_Dialogue; } }
+
+}
